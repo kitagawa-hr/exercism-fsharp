a4992b9e549a4e6a0b9947cea55df0f1aa8c8a4d
kitagawa-hr <kitagawa@cancerscan.jp>
2019-11-01 12:01:44 +0900

    Solved prime-factors

---
+++ prime-factors/PrimeFactors.fs
@@ -0,0 +1,14 @@
+﻿module PrimeFactors
+
+let minFactor (number: int64) =
+    let s = int (sqrt (double number))
+    seq { 2 .. s }
+    |> Seq.filter (fun i -> number % int64 (i) = 0L)
+    |> Seq.tryHead
+
+let rec factors number =
+    let m = minFactor number
+    match m with
+    | Some x -> [ x ] @ factors (number / int64 (x))
+    | None when number > 1L -> [ int number ]
+    | _ -> []
