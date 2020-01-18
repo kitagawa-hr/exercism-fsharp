2e595d9368c3c0aab998930a72ec1b948e2b4b65
Haruki Kitagawa <kitagawahr@gmail.com>
2020-01-18 21:37:06 +0900

    Add collatz-conjecture

---
+++ collatz-conjecture/CollatzConjecture.fs
@@ -0,0 +1,7 @@
+﻿module CollatzConjecture
+
+let rec steps (x: int): int option =
+    if x <= 0 then None
+    elif x =1 then Some 0
+    elif x % 2 = 0 then steps (x / 2) |> Option.map (fun y -> y + 1)
+    else steps (3 * x + 1) |> Option.map (fun y -> y + 1)
